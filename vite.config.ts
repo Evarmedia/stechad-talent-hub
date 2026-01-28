2bcfe785096ef97af78659357a304cd3fee7085c
Evarmedia <mishakmanuel@gmail.com>
2026-01-28 14:48:11 +0100

    chore: update build script for production and upgrade dependencies
    - Modified the build script in package.json to set NODE_ENV to production.
    - Upgraded react and react-dom to version 19.2.4.
    - Added @vitejs/plugin-react as a dependency.
    - Changed the Vite plugin import from @vitejs/plugin-react-swc to @vitejs/plugin-react.
    - Cleaned up the Vite config by removing unused imports and commented-out code.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react';
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,12 +11,19 @@ export default defineConfig(({ mode }) => ({
       "stechad-talent-hub.onrender.com", // Add this host
     ],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean,
-  ),
+  plugins: [react(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
